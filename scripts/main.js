42fbcd3dcab4e0ee56ecd183dfc74091d4d73e0c
sfhjhsdfg <166469022+sfhjhsdfg@users.noreply.github.com>
2024-10-06 16:58:04 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
+const navItems = document.querySelectorAll('.nav-item');//导航栏跳跃效果
 
+        navItems.forEach(item => {
+            item.addEventListener('mouseenter', () => {
+                item.style.animation = 'bounce 0.5s ease';
+            });
+
+            item.addEventListener('animationend', () => {
+                item.style.animation = '';
+            });
+        });
